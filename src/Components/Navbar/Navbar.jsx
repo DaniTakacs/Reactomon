b2998328b23d91b241850df88155f082de0d22e0
Dániel Takács <takacs.d24@gmail.com>
2020-07-21 12:47:30 +0200

    Navbar base created

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = (props) => {
+  return (
+    <div>
+      <button>Pokemons</button>
+      <button>Types</button>
+    </div>
+  );
+};
+
+export default Navbar;
